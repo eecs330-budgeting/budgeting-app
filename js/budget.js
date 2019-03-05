e85cc38f41e804d9cee0462f7189bfff0e765f76
tcbvilla <tcbvilla96@gmail.com>
2019-03-04 23:13:24 -0600

    updated a function to change budget

--- js/budget.js
+++ js/budget.js
@@ -20,6 +20,19 @@ function activate(cur) {
     }
 }
 
+function submit(){
+    var list = $("input[name='purchaseType']:checked").map(function () {
+        return this.value;
+    }).get();
+    let cate = list
+    let memo = $( '#memo' ).val()
+    let cost = $( '#cost' ).val()
+    if( typeof memo !== 'undefined' ) {
+        $( 'ul.purchase-each' ).append( '<li class="card-purchases"><div class="purchases-header"><h3><div class="purchase-content"><span class="icon"><i class="fas fa-shopping-cart"></i></span><div class="purchase-title"><span>'+cate.toString()+'</span><span> $ '+cost+'</span></div></div><span><a href="#" class="btn" data-toggle="modal" data-target="#editModal"><i class="far fa-edit"></i></a></span></h3></div></li>' )
+    }
+    $('#addModal').modal('hide');
+}
+
 
 
 
